a48e1f200e81f87da3d23b7b91064e23f04e6359
nnnn <50388570+nourshinnawi@users.noreply.github.com>
2020-11-20 00:35:43 -0800

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,15 @@
 
+// Assignment #2
+// Nour Shinnawi
+// 11/12/20
+// CS 3650.01
+
+package a2;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        
+        AdminControlPanel.getInstance();
+    }
+}
